0f08e4b280367a7e59c4a4aa3fb38a77c3a5bdf8
Jade Piramide <jadepiramide_pogi@yahoo.com>
2018-11-30 22:37:18 -0800

    Edited to follow .editorconfig

--- src/fizzbuzz.hs
+++ src/fizzbuzz.hs
@@ -1,3 +1,5 @@
+-- Jiramide, 11/30/18
+
 import Data.Maybe (fromMaybe)
 import Data.Foldable (fold)
 import Control.Monad (mapM_)
@@ -6,18 +8,18 @@ type Rule b = b -> Maybe String
 
 rule :: (Integral b) => b -> String -> Rule b
 rule m s i =
-  if i `mod` m == 0
-  then Just s
-  else Nothing
+	if i `mod` m == 0
+	then Just s
+	else Nothing
   
 rules :: (Integral b) => [Rule b]
 rules =
-  [ rule 3 "Fizz"
-  , rule 5 "Buzz"
-  ]
+	[ rule 3 "Fizz"
+	, rule 5 "Buzz"
+	]
   
 fizzBuzz :: (Show b, Integral b) => b -> [String]
 fizzBuzz = fmap (fromMaybe <$> show <*> mRules) . enumFromTo 1
-  where mRules = fold rules
+	where mRules = fold rules
   
 main = mapM_ putStrLn $ fizzBuzz 100
